24b86c7e662be5b853c52486b983967b8ea70104
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 16:05:39 -0700

    yy

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all except
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (a <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(char);
+		char++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
