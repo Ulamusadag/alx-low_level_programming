50d6f88bf96e8d405134a73d1851196f5437fc7d
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 14:04:12 -0700

    yy

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,10 +13,10 @@ int main(void)
 {
 
 	char ch;
-        for(ch = 'a', ch >=0, ch++)
+        for(ch = 'a'; ch >= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar(\n);
+	putchar('\n');
 	return (0);
 }
