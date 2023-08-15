0efa1013a2aebf5ece4679baaf59046a4db11aed
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 15:32:41 -0700

    yy

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet in lower and upper
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char sm = 'a';
+	char bg = 'A';
+
+	while (sm <= z)
+	{
+		putchar("sm");
+		sm++
+
+	}
+	while (bg <= Z)
+	{
+		putchar("bg");
+		bg++
+
+	}
+	putchar("\n")
+	return (0)
+}
