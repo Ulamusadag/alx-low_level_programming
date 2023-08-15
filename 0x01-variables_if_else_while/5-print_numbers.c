37dca272f17f22aa3502af805e67997d5165b306
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 16:34:18 -0700

    yy

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - End point
+ *
+ * Description: sdndtyfyt
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		print("%d", num);
+		num++;
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
