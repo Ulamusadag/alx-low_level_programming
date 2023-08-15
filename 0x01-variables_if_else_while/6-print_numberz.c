edc20060d4681141feacb4b4dec7682f3950e5ea
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 16:54:33 -0700

    yy

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - End point
+ *
+ * Description: dgndjdtj
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++
+
+	}
+	putchar('\n');
+	return (0);
+
+}
