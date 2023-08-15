573285a39b00bdd78187fdc76164f1b9a3d9014f
Ulamusadag <ulamusadag@gmail.com>
2023-08-15 13:52:48 -0700

    yy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - End point
+ *
+ * Description: ghfdfd
+ *
+ * Return: 0 (success)
+ *
+*/
+
+int main(void)
+{
+
+	char ch;
+        for(ch = 'a', ch >=0, ch++)
+	{
+		putchar(ch);
+	}
+	putchar(\n);
+	return (0);
+}
