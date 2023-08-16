a173e663f3b5a63d3368ffbc3d4cedc30078fafa
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 16:38:39 -0700

    yy

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - jygyjg
+ *
+ * @n:ghfjftm
+ *
+ * Return: always 0 (success)
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+
+}
