8e417fa1c6bd97c176189b78dc5d4dfae8af87af
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 15:04:47 -0700

    yyy

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * isalpha - vvh
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+
+}
