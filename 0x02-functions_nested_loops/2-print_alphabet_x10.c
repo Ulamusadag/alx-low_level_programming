686ed9c93a92576439af9bf351e94be8a8888add
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 13:25:11 -0700

    yy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - hjggf
+*/
+
+void print_alphabet_x10(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+}
+
