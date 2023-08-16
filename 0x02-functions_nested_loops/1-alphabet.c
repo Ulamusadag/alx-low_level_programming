786729b074537f0b8dc9a815cd349d75b6c4eed5
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 11:52:33 -0700

    yy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - End point
+ *
+ * Description: hfnndndg
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+	return (0);
+
+}
