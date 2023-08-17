c20dfa75e739d9b77effe00f0c8535b48c658cbc
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 19:54:05 -0700

    yy

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - fhfg
+ *
+ *
+ *
+ * Return: Always 0.
+ */
+
+int print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for(count = n; count > 98; count--)
+			printf("%d ", count);
+	else
+		for(count - n; count < 98; count++)
+			printf("%d ", count);
+	printf("98\n")
+}
