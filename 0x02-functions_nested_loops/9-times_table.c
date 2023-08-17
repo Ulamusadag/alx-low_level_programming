f8817e13ab67db8bfda86488b7b4a85323c474b2
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 19:16:26 -0700

    yy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,24 @@
 
 void times_table(void)
 {
-	int r,c;
+	int num, mult, prod;
 
-	for (c = 0; c <= 9; c++)
+	for (num = 0; num <=9; num++)
 	{
-		for (r = 0; r <= 9; r++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
 		{
-			
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
+		putchar('\n')
 	}
 }
