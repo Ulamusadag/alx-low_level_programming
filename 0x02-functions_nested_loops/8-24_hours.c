63a92f05829abe5ecc3da578507b4f0a2b01d5af
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 18:19:34 -0700

    yy

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,17 +11,17 @@
 
 void jack_bauer(void)
 {
-	init min, hr;
+	int min, hr;
 
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59 ; min++)
 		{
-			_putchart((hr / 10) + 48);
-			_putchart((hr % 10) + 48);
-			_putchart(':');
-			_putchart((min / 10) + 48);
-			_putchart((min % 10) + 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 
 		}
 	}
