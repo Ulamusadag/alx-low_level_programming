67440c42e4f01e846e8d0e5867ae22a17bce8955
Ulamusadag <ulamusadag@gmail.com>
2023-08-16 17:09:44 -0700

    yy

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - ghfdthj
+ *
+ * @n: ryjutyftydht
+ *
+ * Return: lastdigit
+*/
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	if (n < 0)
+		lastdigit = -1 * (n % 10);
+	else
+		lastdigit = n % 10;
+
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+
+}
