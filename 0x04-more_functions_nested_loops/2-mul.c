35ca49839747e5238d6c4401219fb5053f841ea6
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 01:49:41 -0700

    yyy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - ghfgdn
+ * @a: ghddbh
+ * @b: ghmfgm
+ * Return: 0 or 1
+*/
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return(sum);
+}
