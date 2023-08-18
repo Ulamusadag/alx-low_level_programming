290e010815205a9188617909fc590a354bc32db7
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 05:12:08 -0700

    yy

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*more_numbers - jhfjf,g
+*Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
