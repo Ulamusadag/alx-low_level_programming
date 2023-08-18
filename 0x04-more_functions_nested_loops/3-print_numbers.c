3c9b46f38f542533abaa951306e9758adebd4973
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 03:34:54 -0700

    yy

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_numbers - hfjdrhet
+ *Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		_putchar(i + 48);
+	}
+	-putchar('\n');
+}
