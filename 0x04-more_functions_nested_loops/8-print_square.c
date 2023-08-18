8561b57991dd6d537158097b35b67a94f263672e
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 06:28:27 -0700

    yy

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_square - ghfgfg
+*@n: ghfgnf
+*Return: 0
+*/
+
+void print_line(int size)
+{
+	int i;
+
+	for (i = 0; i <= size - 1; i++)
+	{
+		for (i =0; i <= size - 1; i++)
+		{
+			if (size <= 0)
+			{
+				_putchar('\n');
+			}
+			_putchar(35);
+		}
+	}
+	_putchar('\n');
+}
