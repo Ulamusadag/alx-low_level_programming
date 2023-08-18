7ac85a1c69f58a256cbabdfbed930640a0e89a20
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 04:52:43 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_most_numbers - jkggyu
+*Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++
+		}
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
