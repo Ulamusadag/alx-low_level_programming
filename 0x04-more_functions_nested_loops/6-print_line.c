61c78ca459f5ccecc830c9fe48c9111714e105db
Ulamusadag <ulamusadag@gmail.com>
2023-08-18 05:51:48 -0700

    yy

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_line - ghfgfg
+*@n: ghfgnf
+*Return: 0
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar(95);
+	}
+	_putchar('\n');
+}
