a4425d4986c7678e0c6353ecc9c63f1224e9ab02
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 06:02:44 -0700

    yy

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - dfe
+ * @str: gfg
+ * Return: 0
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; str++)
+	{
+		_putchar(*str + 48);
+		i++;
+	}
+	_putchar('\n');
+}
