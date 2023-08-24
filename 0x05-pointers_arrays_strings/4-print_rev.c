353c050af47934616149fd0be65d46bbe6f434c9
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 10:39:15 -0700

    yy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - hj,,jg
+ * @s: gfm
+ * Return: 0
+*/
+
+void print_rev(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '\0' - 1);
+		str--;
+	}
+	_putchar('\n');
+}
