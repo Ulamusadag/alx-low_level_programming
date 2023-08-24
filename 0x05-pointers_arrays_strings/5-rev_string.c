eabf2021377ebf2fa9d6c01f8464f74abb9487e6
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 11:45:34 -0700

    yy

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - chc
+ * @s: gtu
+ * Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+}
