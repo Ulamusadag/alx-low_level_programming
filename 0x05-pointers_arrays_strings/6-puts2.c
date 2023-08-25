c682bc64fb30e2e51e5d702483a9ca6950c2c4c4
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 18:10:40 -0700

    yy

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * puts2 - tfued
+ * @str: gkftyi
+ * Return: 0
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i / 2 != 0; i++)
+	{
+		_putchar(str[i]);
+	
+	}
+}
