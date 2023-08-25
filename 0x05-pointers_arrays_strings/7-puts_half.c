f1f17c595d0a5f0046d6a5686664c035522364fe
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 19:44:01 -0700

    yyy

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *puts_half - ghfgd
+*@str: jhgf
+*Return: 0
+*/
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	if (len % 2 != 0)
+	{
+		for (i = len / 2 + 1; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+}
