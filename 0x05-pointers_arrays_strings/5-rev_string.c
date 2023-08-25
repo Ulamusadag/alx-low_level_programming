38f4e329e0b0bad8d17cc6096db514a6398f33e9
Ulamusadag <ulamusadag@gmail.com>
2023-08-24 17:23:14 -0700

    yy

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,14 @@
 void rev_string(char *s)
 {
 	int len = strlen(s);
-	int i;
+	int i, j;
+	char temp;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
-		_putchar(s[i]);
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+
 	}
-	_putchar('\n');
 }
