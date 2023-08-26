78a961017d9a9ad5a9e02da6b7cc1c34b7f3d84a
Ulamusadag <ulamusadag@gmail.com>
2023-08-26 04:11:12 -0700

    yy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
-	while (src[j] != '\0' && j < n)
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src[j];
+		dest[i] = src[i];
 		i++;
 	}
 	while ( i < n)
