338b251646c1b1dffd22071d881cbe9a81408262
Ulamusadag <ulamusadag@gmail.com>
2023-08-26 06:51:05 -0700

    yy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - fdhet
+ * @a: fgdxgf
+ * @n: jhgh
+ * return: 0
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+}
