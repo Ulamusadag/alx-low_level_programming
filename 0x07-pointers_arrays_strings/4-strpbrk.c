8f3a6226969b8c6a53f5c8eef378356b67b31262
Ulamusadag <ulamusadag@gmail.com>
2023-08-29 13:48:42 -0700

    yy

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - htfrgw
+ * @s: fnddfn
+ * @accept: gmhx
+ * Return: s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	return (0);
+}
