e7451fa1d0dd33db47cc487204ad7d6cc60ad02d
Ulamusadag <ulamusadag@gmail.com>
2023-08-31 03:41:01 -0700

    yy

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - fgdfgdf
+ * @n: fgfg
+ * @val:gyyt
+ * Return: hghfg
+*/
+int sqrt(int n, val);
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+
+}
+
+/**
+ * sqrt - jjhgh
+ * @n: nhgvh
+ * @val: hbjhb
+ * Return: gjf
+*/
+
+int sqrt(int n, val)
+{
+	if (val *val == n)
+		return (val);
+	else if (val *val < n)
+		return (sqrt(n, val + 1));
+	else
+		return (-1);
+}
