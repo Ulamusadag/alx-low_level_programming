2670dad636f4339a13d701cecb36ecdab7b2f118
Ulamusadag <ulamusadag@gmail.com>
2023-09-01 14:45:18 -0700

    yy

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - fgcd
+ * @argc: jhg
+ * @argv: gv
+ * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}
