8365d160351d7d8e2303d7f70143799384e204e6
Ulamusadag <ulamusadag@gmail.com>
2023-09-01 14:27:57 -0700

    yy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
  * main - gfyg
  * @argc: hng
  * @argv: hjj
+ * Return:0
 */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
-	return(0);
+	return (0);
 }
