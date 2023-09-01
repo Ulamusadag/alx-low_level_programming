564dbb41e9e03fbb8e8b61ac69678eb7ca5e1a74
Ulamusadag <ulamusadag@gmail.com>
2023-09-01 14:56:17 -0700

    yy

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - ffg
+ * @argc: hggj
+ * @argv: jhghg
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
