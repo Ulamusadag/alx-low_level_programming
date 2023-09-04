24de6202dd5bc4f4e5949caba8f4a9de0a9d0e43
Ulamusadag <ulamusadag@gmail.com>
2023-09-04 00:46:24 -0700

    yy

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - gjjh
+ * @size: bhg
+ * @c: hj
+ * Return: n
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = (char *) malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+	*(p + i) = '\0';
+
+	return (p);
+}
