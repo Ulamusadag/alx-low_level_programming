42bd73053be38416b06afd3938758cc81aacccad
Ulamusadag <ulamusadag@gmail.com>
2023-09-04 19:11:14 -0700

    yy

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(sizeof(int *) * width);
+		arr[i] = (int *) malloc(sizeof(int) * width);
 
 		if (arr[i] == NULL)
 		{
