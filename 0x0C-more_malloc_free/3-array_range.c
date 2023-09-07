4fc92c2606615393a65ffa69ffb91eedd385162c
Ulamusadag <ulamusadag@gmail.com>
2023-09-07 12:35:41 -0700

    yy

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,11 +12,12 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc(sizeof(int) * (max - min + 1));
+	size = (max - min) + 1;
+	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i <= max; i++)
