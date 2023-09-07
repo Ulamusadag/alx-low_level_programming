ba399458f65b9418cdd6079bacdc261dcd95f4a4
Ulamusadag <ulamusadag@gmail.com>
2023-09-07 11:59:14 -0700

    yy

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
-	if (nmemb == NULL || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
