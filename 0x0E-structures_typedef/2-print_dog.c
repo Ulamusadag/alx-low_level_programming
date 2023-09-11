dbec0a5e9af9e9da74fce11f3fe4c7ba410abf2c
Ulamusadag <ulamusadag@gmail.com>
2023-09-11 11:05:21 -0700

    yy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,11 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name ? d->name : "(nill)");
-	printf("Age: %f\n", d->age ? d->age : "(nill)");
-	printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nill)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	}
 
 }
