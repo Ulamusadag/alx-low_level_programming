b2d5131979ef1278824ef7c89ba4647efdd4cab7
Ulamusadag <ulamusadag@gmail.com>
2023-09-11 04:26:35 -0700

    yy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: hkgjh
+ * @age: gjfhf
+ * @owner: gjhg
+ *
+ * Description: hjgjh
+*/
+
+struct dog 
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif /* DOG_H */
