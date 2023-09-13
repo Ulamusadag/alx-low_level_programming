8bc01608dfbc1f5ef4aeb5b88e3792b62530e378
Ulamusadag <ulamusadag@gmail.com>
2023-09-13 13:00:34 -0700

    yy

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - hbhb
+ * @a: ghgh
+ * @b: jhgjhgj
+ * Return: gjhg
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - fjygh
+ * @a: jhgj
+ * @b: hjj
+ * Return: jhgjhg
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - ggh
+ * @a: khjhj
+ * @b: uhhj
+ * Return: jhkh
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - yg
+ * @a: mghghjj
+ * @b: yjfh
+ * Return: ghg
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gjgjm
+ * @a: mh
+ * @b: hjggjjh
+ * Return: hkh
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
