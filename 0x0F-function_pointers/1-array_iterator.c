7b23622407a69c5cf277b4652dab375cc28e144b
Ulamusadag <ulamusadag@gmail.com>
2023-09-13 01:33:57 -0700

    yy

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - gch
+ * @array: bgv
+ * @size: gvgh
+ * @action: hjhhjl
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			action(array[i]);
+		}
+	}
+
+}
