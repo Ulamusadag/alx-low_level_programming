fe4935bb4d44e776f286e549e617f9d36c4c7ffc
Ulamusadag <ulamusadag@gmail.com>
2023-09-13 05:23:38 -0700

    yy

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - hhhghv
+ * @array: ngvgh
+ * @size: ghjh
+ * @cmp:tfhg
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+
+}
