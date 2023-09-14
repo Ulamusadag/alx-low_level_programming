6659b9a0314f29b54f61467e71e08297dfa58cc1
Ulamusadag <ulamusadag@gmail.com>
2023-09-14 11:23:54 -0700

    yy

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
-		if (str == NULL)
+		if (!str)
 			str = "(nill)";
 		if (!separator)
 			printf("%s", str);
