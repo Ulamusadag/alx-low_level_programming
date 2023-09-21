99d6d62bf61dbdfef125834b64f6f70c1524100c
Ulamusadag <ulamusadag@gmail.com>
2023-09-21 14:53:27 -0700

    yy

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node - guy
+ * @head: johi
+ * @str: hgluhgu
+ * Return: jhgghj
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = str;
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
