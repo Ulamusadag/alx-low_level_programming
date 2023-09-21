e25cceeac02b44db6878865cdd4287eeb74ba81b
Ulamusadag <ulamusadag@gmail.com>
2023-09-21 15:04:42 -0700

    yy

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void first(void) __attribute__((constructor));
+
+/**
+ * first - fdshh
+ * Return: ghfg
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
