59a7b6aa504ef0d48cb8a7d47d793954e4f64d9b
Ulamusadag <ulamusadag@gmail.com>
2023-09-25 13:42:03 -0700

    yy

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - ghfvg
+ * @head: gvgjj
+ * Return: gkg
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t temp = head;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
