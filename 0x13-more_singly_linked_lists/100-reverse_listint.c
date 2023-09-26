60da29d946aa88396dbaebd83a560b73355a869e
Ulamusadag <ulamusadag@gmail.com>
2023-09-26 05:21:06 -0700

    yy

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - ghvhgdser
+ * @head: vgj
+ * Return: gfmgf
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *before = NULL;
+	listint_t *after = NULL;
+
+	while (*head)
+	{
+		after = (*head)->next;
+		(*head)->next = before;
+		before = *head;
+		*head = next;
+	}
+	*head = before;
+
+	return (*head);
+}
