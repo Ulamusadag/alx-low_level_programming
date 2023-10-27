4d91bbe1bf51775bc509a52798f435a4640f607e
Ula Musadag <ulamusadag@gmail.com>
2023-10-27 13:44:52 -0700

    yy

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,27 @@
 unsigned int binary_to_uint(const char *b)
 {
 		unsigned int total, power;
-			int len;
+		int len;
 
-			if (b == NULL)
+		if (b == NULL)
+		{
 			return (0);
+		}
 
-			for (len = 0; b[len]; len++)
+		for (len = 0; b[len]; len++)
+		{
+			if (b[len] != '0' && b[len] != '1')
 			{
-				if (b[len] != '0' && b[len] != '1')
 				return (0);
 			}
+		}
 
-			for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+		for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+		{
+			if (b[len] == '1')
 			{
-				if (b[len] == '1')	
-					total += power;
-			}									}
-
-			return(total);
+				total += power;
+			}
+		}									}
+		return (total);
 }
