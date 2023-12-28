754beca1401ab12823e054fd132b6ff19617197e
Ulamusadag <ulamusadag@gmail.com>
2023-12-27 23:39:37 -0800

    4th

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the list
+ * @head: the head of the list
+ * Return: void
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
+	while (tmp != NULL)
+	{
+		head = head->next;
+		free(tmp);
+		tmp = head;
+	}
+}
